e555b350945b739e1a2a191faa071a3e32cc7b67
Italo Borges <italoborgesevangelista@gmail.com>
2021-11-22 11:00:41 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -3,14 +3,14 @@ package com.educandoweb.course.services;
 import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
-import com.educandoweb.course.resources.exceptions.DatabaseException;
 
 import services.exceptions.ResourceNotFoundException;
 
@@ -39,15 +39,20 @@ public class UserService {
 			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
-		}catch(DataIntegrityViolationException e) {
-			throw new DatabaseException(e.getMessage());
+		}catch(EntityNotFoundException e) {
+			
 		}
 	}
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getOne(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		}catch(RuntimeException e) {
+			e.printStackTrace();
+		}
+		throw new ResourceNotFoundException(id);
 	}
 
 	private void updateData(User entity, User obj) {
